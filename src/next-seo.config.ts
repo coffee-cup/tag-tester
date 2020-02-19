61390f41b0260865425bc0a69a2d40e2a93fef56
Jake Runzer <jakerunzer@gmail.com>
2020-02-19 13:56:51 +0000

    add open graph image

--- src/next-seo.config.ts
+++ src/next-seo.config.ts
@@ -1,9 +1,10 @@
 import { DefaultSeoProps } from "next-seo";
 
 const title = "Tag Tester";
-const url = "https://tag-tester.now.sh/";
+const url = "https://tagtester.dev";
 const description =
   "Test your websites html, open graph and twitter metadata tags.";
+const image = "https://tagtester.dev/tagtester.png";
 
 const config: DefaultSeoProps = {
   title,
@@ -12,6 +13,13 @@ const config: DefaultSeoProps = {
     type: "website",
     url,
     site_name: title,
+    images: [
+      {
+        url: image,
+        width: 1024,
+        height: 1024,
+      },
+    ],
   },
   twitter: {
     handle: "@jakerunzer",
