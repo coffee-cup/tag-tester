8e74e457a047475b15c3afa434a6956df9cba63d
Jake Runzer <jakerunzer@gmail.com>
2020-02-19 20:48:11 +0000

    update title dynamically

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
 import { DefaultSeo } from "next-seo";
 import defaultConfig from "../next-seo.config";
+import { useOG } from "../context";
+import Head from "next/head";
 
-const SEO: React.FC = () => <DefaultSeo {...defaultConfig} />;
+const SEO: React.FC = () => {
+  const { results, url } = useOG();
+
+  return (
+    <>
+      <DefaultSeo {...defaultConfig} />
+
+      {results.type === "success" && (
+        <Head>
+          <title>
+            {url} - {defaultConfig.title}
+          </title>
+        </Head>
+      )}
+    </>
+  );
+};
 
 export default SEO;
